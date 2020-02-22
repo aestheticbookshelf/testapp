c6a43fd1aa17047bfce69df1387807728a4942f8
aestheticbookshelf <aestheticbookshelf@gmail.com>
2020-02-22 12:50:29 +0100

    Provide default for user

--- resources/client/nodejs/index.js
+++ resources/client/nodejs/index.js
@@ -1,9 +1,11 @@
 const { div, Button } = require('@aestheticbookshelf/smartdom')
 
+const USER = PROPS.USER || {}
+
 let app = div().a(
     Button("Login with lichess", () => document.location.href = "/auth/lichess"),
     Button("Login with lichess-bot", () => document.location.href = "/auth/lichess/bot"),
-    div().html(`user id: ${PROPS.USER.id}`)
+    div().html(`user id: ${USER.id}`)
 )    
 
 document.querySelector("#root").appendChild(app.e)
